45a087afe1d1ff9daa9aa3a56068fc9404b2b85d
Lautaro Cruz Srur <lautaro.cruzsrur@gmail.com>
2023-05-06 19:55:16 -0300

    se agrego las vista de los objetos

--- source/js/addItem.js
+++ source/js/addItem.js
@@ -4,39 +4,67 @@ const precioProd = document.getElementById("precio");
 const descripProd = document.getElementById("descripcion");
 const urlProd = document.getElementById("url");
 const botonEnviar = document.getElementById("submit");
+const table = document.getElementById("datetable");
+
+let productos = [];
 
 console.log(`El nombre del producto es ${nombreProd}`);
 console.log("Hola esto es un console");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  function random() {
-    let arraycode=[];
-    while (arraycode.length < 8) {
-      let codigo = parseInt(Math.random() * 99999);
-      if (!arraycode.includes(codigo)) {
-        arraycode.push(codigo);
-      }
-    }
-    return arraycode;
-  }
-  function numeroUnico(){
-    let array = random();
-    let unico;
-    // for(let i = 0 ; i <= array.length; i++){
-    //    unico = array[i];
-    // }
-    unico = array[array.length-1];
-    console.log(`Hola ${unico}`);
-    return unico;
-  }
-    const productos = {
-        nombre: nombreProd.value ,
-        precio: precioProd.value ,
-        descripcion: descripProd.value ,
-        imagen: urlProd.value,
-        codigo: numeroUnico(),
+    e.preventDefault();
+    let nombre = nombreProd.value;
+    let precio = precioProd.value;
+    let descripcion = descripProd.value;
+    let imagen = urlProd.value;
+    let uuid = generateUUIDv4();
+
+    const producto = {
+        titulo: nombre ,
+        precio: precio ,
+        informacion: descripcion ,
+        foto: imagen,
+        codigo: uuid,
+        favorito: false,
     };
-    
+       productos.push(producto);
+    localStorage.setItem("productos", JSON.stringify(productos));
     console.log("producto nuevo: ", productos);
+   productview();
 });
+
+
+function productview(){
+  table.querySelector("tbody").innerHTML = "";
+  productos.forEach((producto) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${producto.titulo}</td>
+    <td>${producto.precio}</td>
+    <td>${producto.informacion}</td>
+    <td>
+    <button class="editar" data-id="">Editar</button>
+    <button class="eliminar" data-id="">Eliminar</button> 
+    </td>
+  `;
+    table.querySelector("tbody").appendChild(tr);
+  });
+
+}
+
+function generateUUIDv4() {
+  let uuid = "", randomValue;
+  for (let i = 0; i < 32; i++) {
+    randomValue = (Math.random() * 16) | 0;
+    if (i == 8 || i == 12 || i == 16 || i == 20) {
+      uuid += "-";
+    }
+    uuid += (i == 12 ? 4 : i == 16 ? (randomValue & 3) | 8 : randomValue).toString(
+      16
+    );
+  }
+  return uuid;
+}
+
+// Ejemplo de uso
+
