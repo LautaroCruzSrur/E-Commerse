421496c71b877051ead102517f3954173d599f10
Lautaro Cruz Srur <lautaro.cruzsrur@gmail.com>
2023-05-07 23:42:32 -0300

    funcionalidad del crud v1

--- source/js/addItem.js
+++ source/js/addItem.js
@@ -12,39 +12,81 @@ console.log(`El nombre del producto es ${nombreProd}`);
 console.log("Hola esto es un console");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let nombre = nombreProd.value;
-    let precio = precioProd.value;
-    let descripcion = descripProd.value;
-    let imagen = urlProd.value;
-    let uuid = generateUUIDv4();
+  e.preventDefault();
+  let titulo = nombreProd.value;
+  let precio = precioProd.value;
+  let informacion = descripProd.value;
+  let foto = urlProd.value;
+  const modo = form.dataset.mode;
+  const editId = form.dataset.editId;
 
-    const producto = {
-        titulo: nombre ,
-        precio: precio ,
-        informacion: descripcion ,
-        foto: imagen,
-        codigo: uuid,
-        favorito: false,
-    };
-       productos.push(producto);
-    localStorage.setItem("productos", JSON.stringify(productos));
-    console.log("producto nuevo: ", productos);
-   productview();
+  if (modo === "add") {
+    let codigo = generateUUIDv4();
+  const producto = {
+    titulo,
+    precio,
+    informacion,
+    foto,
+    codigo,
+    favorito: false,
+  };
+     productos.push(producto);
+     console.log("producto nuevo: ", productos);
+     
+  } else if (modo === "editar") {
+    const index = productos.findIndex((producto) => producto.codigo === editId); // Buscamos el indice del producto a editar
+    if (index !== -1) {
+      // Si el producto existe
+      const product = productos[index]; // Obtenemos el producto a editar del array
+      product.titulo = titulo;
+      product.precio = precio;
+      product.informacion = informacion;
+      product.foto = foto;
+      console.log("1");
+    }
+  }
+  form.reset(); // Reseteamos el formulario
+  form.dataset.mode = "add"; // Cambiamos el modo del boton
+  botonEnviar.textContent = "enviar";
+  productview();
 });
 
-function productEdit(boton){
-  let codigoID = boton.id;
-  const producto = productos.find((producto) => producto.id === codigoID); // Buscamos el producto a editar
 
-  if (producto) {
-    document.getElementById("nombre").value = producto.titulo; // Seteamos el valor del input nombre
-    document.getElementById("precio").value = producto.precio; // Seteamos el valor del input precio
-    document.getElementById("descripcion").value = producto.informacion; // Seteamos el valor del input descripcion
-  }
+
+table.addEventListener("click", (e) => {
+  if (e.target.classList.contains("editar")) {
+    // Si el elemento clickeado tiene la clase editar
+    const id = e.target.dataset.id; // Obtenemos el id del producto a editar
+    const producto = productos.find((producto) => producto.codigo === id); // Buscamos el producto a editar
+    console.log("2");
+    if (producto) {
+      document.getElementById("nombre").value = producto.titulo; // Seteamos el valor del input nombre
+      document.getElementById("precio").value = producto.informacion; // Seteamos el valor del input precio
+      document.getElementById("descripcion").value = producto.informacion; // Seteamos el valor del input descripcion
+      document.getElementById("url").value = producto.foto;
+
+      form.dataset.mode = "editar"; // Cambiamos el modo del formulario
+      form.dataset.editId = id; // Seteamos el id del producto a editar
+      botonEnviar.textContent = "Editar"; // Cambiamos el texto del boton
+      console.log("3");
     }
+  }
+});
+table.addEventListener("click", (e) => {
+  if (e.target.classList.contains("eliminar")) {
+    const id = e.target.dataset.id; // Obtenemos el id del producto a eliminar
+    const index = productos.findIndex((producto) => producto.codigo === id);
+    console.log("4");
+    if (index !== -1) {
+      productos.splice(index, 1);
+      //agregar la funcion para actualizar la lista de productos.
+      productview()
+      console.log("5");
+    }
+  }
+});
 
-function productview(){
+function productview() {
   table.querySelector("tbody").innerHTML = "";
   productos.forEach((producto) => {
     const tr = document.createElement("tr");
@@ -53,14 +95,23 @@ function productview(){
     <td>${producto.precio}</td>
     <td>${producto.informacion}</td>
     <td>
-    <button class="editar" onclick="productEdit(this)" id="${producto.codigo}">Editar</button>
-    <button class="eliminar" id="${producto.codigo}">Eliminar</button> 
+    <button class="editar" data-id="${producto.codigo}">Editar</button>
+    <button class="eliminar" data-id="${producto.codigo}">Eliminar</button> 
     </td>
   `;
     table.querySelector("tbody").appendChild(tr);
-  });
+    localStorage.setItem("productos", JSON.stringify(productos));
 
+  });
 }
+const obtenerProductos = localStorage.getItem("productos");
+
+if (obtenerProductos) {
+  productos = JSON.parse(obtenerProductos);
+  productview();
+  console.log("6");
+}
+
 
 function generateUUIDv4() {
   let uuid = "", randomValue;
@@ -75,6 +126,4 @@ function generateUUIDv4() {
   }
   return uuid;
 }
-
-// Ejemplo de uso
 
