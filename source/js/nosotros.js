775ad93c843af05fc9137fcb524b6b9ca2e9c442
Lautaro Cruz Srur <lautaro.cruzsrur@gmail.com>
2023-05-11 19:37:48 -0300

    agregando nosotros

---
+++ source/js/nosotros.js
@@ -0,0 +1,22 @@
+const fran = document.getElementById("Fran");
+const lautaro = document.getElementById("Lautaro");
+const pablo= document.getElementById("Pablo");
+const luis= document.getElementById("Luis");
+const lucas= document.getElementById("Lucas");
+
+
+fran.addEventListener("click", (e) =>{
+  Swal.fire('Any fool can use a computer')
+});
+lautaro.addEventListener("click", (e) =>{
+  Swal.fire('hola soy Lautaro tengo 22 , soy estudiante de Rolling Code, Argentina programa y de la Unt programador universitario, me gusta leer y desarmar cosas y el gym')
+});
+pablo.addEventListener("click", (e) =>{
+  Swal.fire('Hola, soy Pablo tengo 27 años, soy estudiante de Programacion FullStack en Rolling Code y Psicologia en UNT, me gusta mucho los videojuegos, el desarrollo de los mismos, me encanta el deporte y soy fanatico de la NBA.')
+});
+luis.addEventListener("click", (e) =>{
+  Swal.fire('Luis, tengo 38 años, trabajo en Inst. Maternidad y soy técnico en Estadísticas de la  UNT. Hobbyist: "Ajedrez", "Fútbol", "Lectura: ciencia, psicología,  sociología" y "tecnología"')
+});
+lucas.addEventListener("click", (e) =>{
+  Swal.fire('Any fool can use a computer')
+});
