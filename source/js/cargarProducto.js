6887170f0952bd87f84d82007bc958b2817f0ad1
Lucasybanez <loquendero31@gmail.com>
2023-05-14 17:32:07 -0300

    Se avanzó sobre la función para mostrar un producto de acuerdo a su código

--- source/js/cargarProducto.js
+++ source/js/cargarProducto.js
@@ -1,21 +1,34 @@
+//localStorage.setItem("productoDetalle", "248861ee-a8f1-4aab-8cde-e39581b1dfcc");
 let nombre = document.getElementById("nombreP");
 let precio = document.getElementById("precioP");
 let categoria = document.getElementById("categoriaP");
 let descripcion = document.getElementById("descripcionP");
+let foto = document.getElementById("imagenP");
+let producto = recuperarProducto(localStorage.getItem("productoDetalle"));
 
-console.log(recuperarProductos());
-cargarProducto();
 
-function recuperarProductos(){
+cargarProducto(producto);
+
+function recuperarProducto(codigo){
+
+    //Recupera todos los productos
     let todosProductos= JSON.parse(localStorage.getItem("productos"));
-    return todosProductos[0];
+
+    let productoEncontrado;
+    
+    //busca el producto solicitado
+    todosProductos.forEach(function(todosProductos) {
+        if (todosProductos.codigo === codigo) {
+          productoEncontrado = todosProductos;
+        }
+      });
+      return productoEncontrado;
 }
 
-function cargarProducto(){
-    let producto = recuperarProductos();
-
+function cargarProducto(producto){
     nombre.innerHTML = producto.titulo;
     precio.innerHTML = "$" + producto.precio;
     categoria.innerHTML =  producto.categoria;
     descripcion.innerHTML = producto.informacion;
+    foto.innerHTML = `<img src="${producto.foto}" class="d-block w-100" alt=''>`
 }
